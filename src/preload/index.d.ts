8648ec0933c46e8d2e783c38b8012d5060a6b375
mncolla <maxincolla@gmail.com>
2025-04-08 13:30:44 -0300

    feat: create a custom browser api

--- src/preload/index.d.ts
+++ src/preload/index.d.ts
@@ -1,8 +1,12 @@
 import { ElectronAPI } from '@electron-toolkit/preload'
 
+interface API {
+    executeCode: (code: string) => Promise<{ success: boolean; result?: any; error?: string }>;
+}
+
 declare global {
   interface Window {
     electron: ElectronAPI
-    api: unknown
+    api: API
   }
 }
