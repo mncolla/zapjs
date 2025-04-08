a02ab833c591d9070b5cde397e2f9cd25d795b75
mncolla <maxincolla@gmail.com>
2025-04-07 21:21:40 -0300

    feat: configure vitejs

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,8 +1,8 @@
 import { mount } from 'svelte'
 
 import './assets/main.css'
+import App from './app.svelte'
 
-import App from './App.svelte'
 
 const app = mount(App, {
   target: document.getElementById('app')!
